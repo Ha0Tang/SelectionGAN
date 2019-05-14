815b872efc50c34d9cc98ed35cd5dc6bc71dc9e2
Ha0Tang <haotang@pku.edu.cn>
2019-05-14 11:27:15 +0200

    update readme

---
+++ datasets/download_selectiongan_dataset.sh
@@ -0,0 +1,16 @@
+FILE=$1
+
+if [[ $FILE != "dayton_ablation" && $FILE != "dayton" && $FILE != "cvusa" && $FILE != "ego2top"]]; then
+  echo "Available datasets are dayton_ablation, dayton, cvusa, ego2top"
+  exit 1
+fi
+
+echo "Specified [$FILE]"
+
+URL=http://http://disi.unitn.it/~hao.tang/dataset/SelectionGAN/$FILE.tar.gz
+TAR_FILE=./datasets/$FILE.tar.gz
+TARGET_DIR=./datasets/$FILE/
+wget -N $URL -O $TAR_FILE
+mkdir -p $TARGET_DIR
+tar -zxvf $TAR_FILE -C ./datasets/
+rm $TAR_FILE
