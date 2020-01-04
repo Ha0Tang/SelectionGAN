4607511c7c09e6b893c0f157eff4d067ccc07bbf
Ha0Tang <haotang@pku.edu.cn>
2020-01-04 15:31:19 +0000

    Update SVA pretrained models

--- scripts/download_selectiongan_model.sh
+++ scripts/download_selectiongan_model.sh
@@ -1,6 +1,6 @@
 FILE=$1
 
-echo "Note: available models are dayton_a2g_256, dayton_g2a_256, cvusa, dayton_a2g_64, dayton_g2a_64, and ego2top"
+echo "Note: available models are dayton_a2g_256, dayton_g2a_256, cvusa, dayton_a2g_64, dayton_g2a_64, ego2top and sva"
 echo "Specified [$FILE]"
 
 URL=http://disi.unitn.it/~hao.tang/uploads/models/SelectionGAN/${FILE}_pretrained.tar.gz
