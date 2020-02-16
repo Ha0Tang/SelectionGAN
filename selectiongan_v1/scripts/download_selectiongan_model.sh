bb1e5168ac6432bfcf8030e7c7cd2370f7c39733
Hao Tang <hao.tang@unitn.it>
2020-02-16 23:27:23 +0000

    Fix typos

--- selectiongan_v1/scripts/download_selectiongan_model.sh
+++ selectiongan_v1/scripts/download_selectiongan_model.sh
@@ -1,6 +1,6 @@
 FILE=$1
 
-echo "Note: available models are dayton_a2g_256, dayton_g2a_256, cvusa, dayton_a2g_64, dayton_g2a_64, ego2top, sva, ntu, senz3d and radbound"
+echo "Note: available models are dayton_a2g_256, dayton_g2a_256, cvusa, dayton_a2g_64, dayton_g2a_64, ego2top, sva, ntu, senz3d and radboud"
 echo "Specified [$FILE]"
 
 URL=http://disi.unitn.it/~hao.tang/uploads/models/SelectionGAN/${FILE}_pretrained.tar.gz
